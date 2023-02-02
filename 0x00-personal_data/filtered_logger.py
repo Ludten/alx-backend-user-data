86e4fb794897446c310a3733c25d4341f71d5cec
Oluwateniola Adegbulugbe <adegbulugbe.teniola@outlook.com>
2023-02-02 14:33:07 +0100

    ALX User Backend Data
    line check

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,6 +4,7 @@ logger module
 """
 
 
+import logging
 import re
 from typing import List
 
@@ -21,3 +22,28 @@ def filter_datum(
         pattern = "(?<={}=).*?(?={})".format(field, separator)
         message = re.sub(pattern, redaction, message)
     return message
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields: List[str]):
+        self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        """
+        format and log records
+        """
+        record.message = filter_datum(
+            self.fields, self.REDACTION, record.getMessage(), self.SEPARATOR)
+        record.asctime = self.formatTime(record, self.datefmt)
+        return self.FORMAT % {"name": record.name,
+                              "levelname": record.levelname,
+                              "asctime": record.asctime,
+                              "message": record.message}
