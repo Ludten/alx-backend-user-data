e202fcc32a760fbc4528664d63680a5b760dee16
Oluwateniola Adegbulugbe <adegbulugbe.teniola@outlook.com>
2023-02-02 23:31:20 +0100

    ALX User Backend Data
    logger (PII_FIELDS)

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,7 +8,7 @@ import logging
 import re
 from typing import List, Tuple
 
-PII_FIELDS: Tuple[str] = ('email', 'phone', 'ssn', 'password', ' ip')
+PII_FIELDS: Tuple[str] = ('email', 'phone', 'ssn', 'password', 'ip')
 
 
 def filter_datum(
