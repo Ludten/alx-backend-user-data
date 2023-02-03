35b248f800aedd02788a39e2bee8d9d8e90757a3
Oluwateniola Adegbulugbe <adegbulugbe.teniola@outlook.com>
2023-02-03 01:42:30 +0100

    ALX User Backend Data
    hash encryption(password validation)

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -14,3 +14,11 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hash = bcrypt.hashpw(bytes, salt)
     return hash
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    check if a password match
+    """
+    bytes = password.encode('utf-8')
+    return bcrypt.checkpw(bytes, hashed_password)
