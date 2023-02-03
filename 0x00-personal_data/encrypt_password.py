0d663f142b7a734d7f1327230a50677b94d6ab5b
Oluwateniola Adegbulugbe <adegbulugbe.teniola@outlook.com>
2023-02-03 01:37:25 +0100

    ALX User Backend Data
    hash encryption

---
+++ 0x00-personal_data/encrypt_password.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+Encrypting passwords module
+"""
+
+import bcrypt
+
+
+def hash_password(password: str) -> str:
+    """
+    hash password
+    """
+    bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hash = bcrypt.hashpw(bytes, salt)
+    return hash
