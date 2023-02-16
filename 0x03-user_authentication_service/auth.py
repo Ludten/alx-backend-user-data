25ccac5adb8aca48640dda7308420dd87b8c3947
Oluwateniola Adegbulugbe <adegbulugbe.teniola@outlook.com>
2023-02-16 14:10:29 +0100

    ALX User Backend Data - User authentication service
    not a method???

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -11,21 +11,22 @@ from user import User
 from uuid import uuid4
 
 
+def _hash_password(password: str) -> bytes:
+    """
+    Hash a password
+    """
+    bytes = password.encode('utf-8')
+    salt = bcrypt.gensalt()
+    hash = bcrypt.hashpw(bytes, salt)
+    return hash
+
+
 class Auth:
     """Auth class to interact with the authentication database.
     """
 
     def __init__(self):
         self._db = DB()
-
-    def _hash_password(self, password: str) -> bytes:
-        """
-        Hash a password
-        """
-        bytes = password.encode('utf-8')
-        salt = bcrypt.gensalt()
-        hash = bcrypt.hashpw(bytes, salt)
-        return hash
 
     def register_user(self, email: str, password: str) -> User:
         """
@@ -35,7 +36,7 @@ class Auth:
             self._db.find_user_by(email=email)
             raise ValueError('User {} already exists'.format(email))
         except NoResultFound:
-            user = self._db.add_user(email, self._hash_password(password))
+            user = self._db.add_user(email, _hash_password(password))
         return user
 
     def valid_login(self, email: str, password: str) -> bool:
