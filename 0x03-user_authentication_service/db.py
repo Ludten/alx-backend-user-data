5368f7cad686d7a9d21ca5a1656395dbad665bfe
Oluwateniola Adegbulugbe <adegbulugbe.teniola@outlook.com>
2023-02-16 14:08:09 +0100

    ALX User Backend Data - User authentication service
    ment

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -17,7 +17,7 @@ class DB:
     def __init__(self) -> None:
         """Initialize a new DB instance
         """
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine("sqlite:///a.db")
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
@@ -31,7 +31,7 @@ class DB:
             self.__session = DBSession()
         return self.__session
 
-    def add_user(self, email: str, hashed_password: bytes) -> User:
+    def add_user(self, email: str, hashed_password: str) -> User:
         """
         Create and return a User instance
         """
